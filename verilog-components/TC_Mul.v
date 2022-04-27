d1f272db0a49a83a835009af1639ecc8b9fd3fcf
Logxen <logxen@hotmail.com>
2022-04-27 16:01:22 -0700

    gave mul 2 output ports

--- verilog-components/TC_Mul.v
+++ verilog-components/TC_Mul.v
@@ -1,9 +1,10 @@
-module TC_Mul (in0, in1, out);
+module TC_Mul (in0, in1, out0, out1);
     parameter size = 1;
     input [size-1:0] in0;
     input [size-1:0] in1;
-    output [2*size-1:0] out;
+    output [size-1:0] out0;
+    output [size-1:0] out1;
 
-    assign out = in0 * in1;
+    assign {out1, out0} = in0 * in1;
 endmodule
 
