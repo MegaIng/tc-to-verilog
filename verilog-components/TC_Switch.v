646dabb7e38ae831cbefe8c8c19c55ef4e8ae3f2
Logxen <logxen@hotmail.com>
2022-04-27 16:08:53 -0700

    fixed switch's sel port

--- verilog-components/TC_Switch.v
+++ verilog-components/TC_Switch.v
@@ -1,13 +1,13 @@
-module TC_Switch(dis, in, out);
+module TC_Switch(sel, in, out);
     parameter size = 1;
-    input dis;
+    input sel;
     input [size-1:0] in;
     tri0 reg [size-1:0] out;
     
     always @ (en or in) begin
         case(en)
-        1'b0 : out <= in;
-        1'b1 : out <= {size{1'bZ}};
+        1'b0 : out <= {size{1'bZ}};
+        1'b1 : out <= in;
         endcase
     end
 endmodule
