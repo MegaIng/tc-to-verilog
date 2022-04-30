8db9f0b04e400230ef2f67a88bacc9b377f52549
Logxen <logxen@hotmail.com>
2022-04-29 22:00:03 -0700

    fixes to the clock system

--- verilog-components/TC_Counter.v
+++ verilog-components/TC_Counter.v
@@ -1,23 +1,29 @@
 module TC_Counter (clk, rst, save, in, out);
     parameter BIT_WIDTH = 8;
-    parameter count = {BIT_WIDTH{1'b1}};
+    parameter count = 1;
     input clk;
     input rst;
     input save;
     input [BIT_WIDTH-1:0] in;
     output reg [BIT_WIDTH-1:0] out;
-    reg [BIT_WIDTH-1:0] value;
+    
+    initial begin
+        out = {BIT_WIDTH{1'b0}};
+    end
     
     always @ (posedge clk or posedge rst) begin
         if (rst) begin
             out <= {BIT_WIDTH{1'b0}};
-            value <= {BIT_WIDTH{1'b0}};
         end else if (save) begin
-            out <= value;
-            value <= in;
+            out <= in;
         end else begin
-            out <= value;
-            value <= value + count;
+            out <= out + count;
+        end
+    end
+    
+    always @ (in) begin
+        if (!rst && save) begin
+            out <= in;
         end
     end
 endmodule
--- verilog-components/TC_Decoder3.v
+++ verilog-components/TC_Decoder3.v
@@ -14,27 +14,17 @@ module TC_Decoder3 (dis, sel0, sel1, sel2, out0, out1, out2, out3, out4, out5, o
 
     always @ (dis or {sel2, sel1, sel0})
     begin
-        if(dis) begin
-            out0 <= 1'b0;
-            out1 <= 1'b0;
-            out2 <= 1'b0;
-            out3 <= 1'b0;
-            out4 <= 1'b0;
-            out5 <= 1'b0;
-            out6 <= 1'b0;
-            out7 <= 1'b0;
-        end else begin
-            case({sel2, sel1, sel0})
-            3'b000 : {out7, out6, out5, out4, out3, out2, out1, out0} =  8'b0000_0001;
-            3'b001 : {out7, out6, out5, out4, out3, out2, out1, out0} =  8'b0000_0010;
-            3'b010 : {out7, out6, out5, out4, out3, out2, out1, out0} =  8'b0000_0100;
-            3'b011 : {out7, out6, out5, out4, out3, out2, out1, out0} =  8'b0000_1000;
-            3'b100 : {out7, out6, out5, out4, out3, out2, out1, out0} =  8'b0001_0000;
-            3'b101 : {out7, out6, out5, out4, out3, out2, out1, out0} =  8'b0010_0000;
-            3'b110 : {out7, out6, out5, out4, out3, out2, out1, out0} =  8'b0100_0000;
-            3'b111 : {out7, out6, out5, out4, out3, out2, out1, out0} =  8'b1000_0000;
-            endcase
-        end
+        case({dis, sel2, sel1, sel0})
+        4'b0000 : {out7, out6, out5, out4, out3, out2, out1, out0} = 8'b0000_0001;
+        4'b0001 : {out7, out6, out5, out4, out3, out2, out1, out0} = 8'b0000_0010;
+        4'b0010 : {out7, out6, out5, out4, out3, out2, out1, out0} = 8'b0000_0100;
+        4'b0011 : {out7, out6, out5, out4, out3, out2, out1, out0} = 8'b0000_1000;
+        4'b0100 : {out7, out6, out5, out4, out3, out2, out1, out0} = 8'b0001_0000;
+        4'b0101 : {out7, out6, out5, out4, out3, out2, out1, out0} = 8'b0010_0000;
+        4'b0110 : {out7, out6, out5, out4, out3, out2, out1, out0} = 8'b0100_0000;
+        4'b0111 : {out7, out6, out5, out4, out3, out2, out1, out0} = 8'b1000_0000;
+        default : {out7, out6, out5, out4, out3, out2, out1, out0} = 8'b0000_0000;
+        endcase
     end 
 endmodule
 
